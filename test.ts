53417bd5f17ab9be85364aff7e49ac01fd838ab0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-03-13 16:08:56 -0700

    Updated typescript definition
    Fixes #46

--- test.ts
+++ test.ts
@@ -1,15 +1,15 @@
-import ArcGIS = require("./terraformer-arcgis-parser");
+import * as TerraformerArcGIS from "./index";
 
-console.assert(typeof ArcGIS !== undefined);
+console.assert(typeof TerraformerArcGIS !== undefined);
 
 // parse an ArcGIS Geometry to GeoJSON
-let geojsonPoint = ArcGIS.parse({
-  "x": -122.6764,
-  "y": 45.5165,
-  "spatialReference": {
-    "wkid": 4326
+let geojsonPoint = TerraformerArcGIS.parse({
+  x: -122.6764,
+  y: 45.5165,
+  spatialReference: {
+    wkid: 4326
   }
 });
 
 // convert a GeoJSON object into an ArcGIS geometry
-let arcgisPoint = ArcGIS.convert(geojsonPoint);
+let arcgisPoint = TerraformerArcGIS.convert(geojsonPoint);
